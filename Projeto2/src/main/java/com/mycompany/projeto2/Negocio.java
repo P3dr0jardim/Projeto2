6e4529ee3cfb6eea115ddceab1fa73b1314792a1
FranciscoBelo18 <96338080+FranciscoBelo18@users.noreply.github.com>
2021-12-21 22:52:28 +0000

    Classe Negocio
    Feito cenas na classe Negocio

---
+++ Projeto2/src/main/java/com/mycompany/projeto2/Negocio.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto2;
+
+/**
+ *
+ * @author franc
+ */
+import java.util.*;
+public class Negocio {
+    private String nome;
+    private double ValorBaseTributavel;
+    private int ProbPolicia;
+    private boolean Policiavel;
+    
+    //Constutor de negocio
+    public Negocio(String nome, double ValorBaseTributavel, int ProbPolicia, boolean Policiavel){
+        this.nome=nome;
+        this.ValorBaseTributavel=ValorBaseTributavel;
+        this.ProbPolicia=0;
+        this.Policiavel=Policiavel;
+    }
+    
+    //Getters e setters necessários
+    public String getNome(){
+        return nome;
+    }
+    public double getValorBaseTributavel(){
+        return ValorBaseTributavel;
+    }
+    public void setValorBaseTributavel(int NovoValorBase){
+        this.ValorBaseTributavel=NovoValorBase;
+    }
+    public boolean VerificaPoliciavel(){
+        return Policiavel;
+    }
+    public int getProbPolicia(){
+        return ProbPolicia;
+    }
+    public void setProbPolicia(int prob){
+        this.ProbPolicia=prob;
+    }    
+    
+    //Probabilidade da policia agir caso haja um informador no negócio
+    public void ProbComInformador(int aumento){
+        ProbPolicia+=aumento;
+    }
+    
+    //Probabilidade do negocio ser atuado pela policia
+    public void PoliciaAtuar(){
+        if (VerificaPoliciavel()==true){
+            Random rand= new Random();
+            int policia = rand.nextInt(61)+20;//valor entre 20 e 80 (inclusive ambos)
+            setProbPolicia(policia);
+        }
+    }
+    
+
+}
